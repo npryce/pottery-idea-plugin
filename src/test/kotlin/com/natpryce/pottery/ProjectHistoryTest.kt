bac2481cb8f47803ebfdd351c6cf37989f26578f
Nat Pryce <sw@natpryce.com>
2018-09-11 17:44:26 +0100

    Testing multiple history events

--- src/test/kotlin/com/natpryce/pottery/ProjectHistoryTest.kt
+++ src/test/kotlin/com/natpryce/pottery/ProjectHistoryTest.kt
@@ -20,13 +20,18 @@ class ProjectHistoryTest {
     
     @Test
     fun `record and read back history`() {
-        history.post(Instant.parse("2018-08-14T17:42:00Z"), POST_TYPE, "some content")
+        history.post(t("2018-08-14T09:00:00Z"), POST_TYPE, "some content")
+        history.post(t("2018-08-14T10:00:00Z"), POST_TYPE, "more content")
         
         val sherds = history.sherds(Span(Instant.parse("2018-08-14T00:00:00Z"), Instant.parse("2018-09-14T00:00:00Z")))
         
         assertThat(sherds, equalTo(listOf(
-            Sherd(type= POST_TYPE, timestamp = Instant.parse("2018-08-14T17:42:00Z"), uid="AAAAAAAAAAAAAABh",
-                file= Paths.get("docs/project-history/2018/2018-08/2018-08-14T17:42:00Z_post_AAAAAAAAAAAAAABh.md"))
+            Sherd(type= POST_TYPE, timestamp = t("2018-08-14T09:00:00Z"), uid="AAAAAAAAAAAAAABh",
+                file= Paths.get("docs/project-history/2018/2018-08/2018-08-14T09:00:00Z_post_AAAAAAAAAAAAAABh.md")),
+            Sherd(type= POST_TYPE, timestamp = t("2018-08-14T10:00:00Z"), uid="AAAAAAAAAAAAAABi",
+                file= Paths.get("docs/project-history/2018/2018-08/2018-08-14T10:00:00Z_post_AAAAAAAAAAAAAABi.md"))
         )))
     }
+    
+    fun t(instantStr: String) = Instant.parse(instantStr)
 }
