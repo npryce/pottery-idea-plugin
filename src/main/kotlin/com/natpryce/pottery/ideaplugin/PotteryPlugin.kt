f746f62d3bcaae5036febebb4c20cb9bc27c377a
Nat Pryce <sw@natpryce.com>
2018-09-12 08:37:08 +0100

    Remove action buttons from title bar

--- src/main/kotlin/com/natpryce/pottery/ideaplugin/PotteryPlugin.kt
+++ src/main/kotlin/com/natpryce/pottery/ideaplugin/PotteryPlugin.kt
@@ -1,14 +1,12 @@
 package com.natpryce.pottery.ideaplugin
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.openapi.wm.ex.ToolWindowEx
 import com.intellij.ui.content.ContentFactory.SERVICE
 import com.natpryce.pottery.ProjectHistory
 import java.time.Clock
@@ -22,13 +20,9 @@ class PotteryPlugin : ToolWindowFactory, DumbAware, Disposable {
         val history = ProjectHistory(IdeaVfsProjectHistoryStorage(project))
         val potteryPanel = PotteryPanel(project, history, clock)
         val fileListener = HistoryRefresher(project, history, potteryPanel::refresh)
-
+        
         toolWindow.contentManager.addContent(SERVICE.getInstance().createContent(potteryPanel, "", false))
         
-        (toolWindow as? ToolWindowEx)?.setTitleActions(DefaultActionGroup(
-            PostSherdAction(project, history, clock)
-        ))
-    
         ApplicationManager.getApplication().getMessageBus().connect(this)
             .subscribe(VirtualFileManager.VFS_CHANGES, fileListener)
         
